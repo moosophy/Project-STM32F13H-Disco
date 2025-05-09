8f3161b5173d96e8eb9d4760831f184a1eb9f344
Katya Yakubava <moosophy@Katyas-MacBook-Air.local>
2025-05-09 17:33:49 +0200

    first commit

---
+++ project/src/main.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/trace.h"
+
+
+
+int
+main(int argc, char* argv[])
+{
+  // At this stage the system clock should have already been configured
+  // at high speed.
+
+  // Infinite loop
+  while (1)
+    {
+       // Add your code here.
+    }
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
